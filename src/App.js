02fbde5ccab393cf0d841681445cb6dbf9bb7291
wladm <wladmyr@visiondevelopment.co>
2020-08-19 19:55:22 -0300

    Challenge Mobile Finished

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import {
   SafeAreaView,
@@ -9,47 +9,77 @@ import {
   StyleSheet,
   TouchableOpacity,
 } from "react-native";
+import api from "./services/api";
 
 export default function App() {
+  const [repositories, setRepositories] = useState([]);
+  const [like, setLikes] = useState(0);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    })
+  }, []);
+
   async function handleLikeRepository(id) {
-    // Implement "Like Repository" functionality
+    const response = await api.post(`repositories/${id}/like`);
+
+    const repositoryLiked = response.data;
+
+    const repositoryAtt = repositories.map(repository => {
+      if (repository.id === id) {
+        return repositoryLiked;
+      } else {
+        return repository;
+      }
+    });
+
+    setRepositories(repositoryAtt);
   }
+
 
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+
       <SafeAreaView style={styles.container}>
-        <View style={styles.repositoryContainer}>
-          <Text style={styles.repository}>Repository 1</Text>
+        <FlatList
+          data={repositories}
+          keyExtractor={repo => repo.id}
+          renderItem={({ item: repository }) => {
+            return <View key={repository.id} style={styles.repositoryContainer}>
+              <Text style={styles.repository}>{repository.title}</Text>
 
-          <View style={styles.techsContainer}>
-            <Text style={styles.tech}>
-              ReactJS
-            </Text>
-            <Text style={styles.tech}>
-              Node.js
-            </Text>
-          </View>
+              <View style={styles.techsContainer}>
+                {repository.techs.map(tech => {
+                  return <Text key={tech} style={styles.tech}>
+                    {tech}
+                  </Text>
+                })}
+              </View>
 
-          <View style={styles.likesContainer}>
-            <Text
-              style={styles.likeText}
-              // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
-              testID={`repository-likes-1`}
-            >
-              3 curtidas
-            </Text>
-          </View>
+              <View style={styles.likesContainer}>
+                <Text
+                  style={styles.likeText}
+                  testID={`repository-likes-${repository.id}`}
+                >
+                  {repository.likes} curtida{repository.likes > 1 ? 's' : ''}
+                </Text>
+              </View>
 
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => handleLikeRepository(1)}
-            // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
-            testID={`like-button-1`}
-          >
-            <Text style={styles.buttonText}>Curtir</Text>
-          </TouchableOpacity>
-        </View>
+              <TouchableOpacity
+                activeOpacity={0.6}
+                style={styles.button}
+                onPress={() => handleLikeRepository(repository.id)}
+                testID={`like-button-${repository.id}`}
+              >
+                <Text style={styles.buttonText}>Curtir</Text>
+              </TouchableOpacity>
+            </View>
+
+          }}
+
+        />
       </SafeAreaView>
     </>
   );
@@ -95,6 +125,7 @@ const styles = StyleSheet.create({
   },
   button: {
     marginTop: 10,
+    borderRadius: 16,
   },
   buttonText: {
     fontSize: 14,
--- src/services/api.js
+++ src/services/api.js
@@ -5,3 +5,12 @@ const api = axios.create({
 });
 
 export default api;
+
+/**
+ * iOS com emulador: localhost;
+ * iOS com físico: IP da máquina;
+ * Android com emulador: localhost: adb reverse tcp:3333 tcp:3333 --> red da maq. pro emulator;
+ * Android com emulador: 10.0.2.2 (Android Studio);
+ * Android com emulador: 10.0.3.2 (Genymotion)
+ * Android com físico: Ip da máquina
+ */
